46b7c357b85fe292475684dba2e29ff623c58105
sihabsafin <sihabulislamsafin1@gmail.com>
2025-06-01 11:38:25 +0600

    our javascript file her

---
+++ khalid.js
@@ -0,0 +1,12 @@
+//our first javascipt here
+console.log("Hello, Khalid!");
+// This is a simple JavaScript program that prints a greeting to the console
+// It demonstrates the basic syntax of JavaScript and how to output text
+// to the console using console.log
+// You can run this code in a web browser's console or in a JavaScript environment
+
+// like Node.js to see the output
+// To run this code, simply copy and paste it into your browser's console or a JavaScript file
+jdk do {
+    
+} while (condition);
